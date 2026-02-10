fe22483dc412d1921dc900b537ecf0f80d1022d2
Marvin Barrera <59856529+MarvinBT1988@users.noreply.github.com>
2026-02-10 22:24:17 +0000

    cambiar el permiso de ge id usuario

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -5,7 +5,7 @@ const auth = require('../middlewares/authMiddleware');
 const checkRole = require('../middlewares/checkRoleMiddleware');
 
 router.get('/', auth, checkRole('ADMIN_ROLE'), userController.getUsers);
-router.get('/:id', auth,checkRole('ADMIN_ROLE'), userController.getUserById);
+router.get('/:id', auth, userController.getUserById);
 router.post('/', auth,checkRole('ADMIN_ROLE'), userController.createUser);
 router.post('/login', userController.login);
 router.put('/:id', auth,checkRole('ADMIN_ROLE'), userController.updateUser);
